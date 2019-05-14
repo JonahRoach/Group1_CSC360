313429f0290eda1d2098e96405584a84f29efeff
JonahRoach <36675649+JonahRoach@users.noreply.github.com>
2019-05-14 12:12:38 -0600

    Header moved to Component
    Created a header component to use on every page. This will create a more cohesive feel for the web pages hopefully.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <app-header></app-header>
+
+
+`,
+styles: []
 })
 export class AppComponent {
   title = 'Group 1';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './components/header/header.component';
+//import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HeaderComponent,
+    //HomepageComponent
   ],
   imports: [
     BrowserModule,
