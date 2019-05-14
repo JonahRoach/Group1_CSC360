7d08c627ba7d14fae29f4a7aad310d81bf62cd65
archie paredes <archieparedes@gmail.com>
2019-05-14 13:38:32 -0500

    wkPLAN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
+import { getPluralCategory } from '@angular/common/src/i18n/localization';
+import { DEFAULT_INTERPOLATION_CONFIG } from '@angular/compiler';
 
+interface Workout {
+  name: string;
+  category: string;
+  duration: string;
+}
+
+const WORKOUTS: Workout[] = [
+  {
+    name: 'Squats',
+    //flag: 'f/f3/Flag_of_Russia.svg',
+    category: 'Strength',
+    duration: '3 set 8 reps'
+  },
+  {
+    name: 'Leg Extention',
+    category: 'Strength',
+    duration: '2 set 10 reps'
+  },
+  {
+    name: 'Leg Curl',
+    category: 'Strength',
+    duration: '2 set 10 reps'
+  },
+  {
+    name: 'Bike',
+    category: 'Cardio',
+    duration: '30 minutes'
+  }
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'Group 1';
+  addHero() {
+    if(this.nam != "" || this.cat != 'Category' || this.dur != "") {
+      WORKOUTS.push({name:this.nam, category:this.cat, duration:this.dur});
+      this.nam = "";
+      this.dur = "";
+      this.cat = "Category";
+    }
+  }
+
+  title = 'w-planner';
+  currentJustify = 'justified';
+  workoutsM = WORKOUTS;
+  nam: string;
+  dur:string;
+  cat: string = "Category";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomepageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule,
     FormsModule,
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
