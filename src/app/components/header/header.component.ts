82f6cdfe0e8141328bc11ac753aae9ae3d9536be
JonahRoach <roachjonah@gmail.com>
2019-05-28 20:04:35 -0500

    FIxed header, updated contact us to Bootstrap 4

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  public isCollapsed = false;
   constructor() { }
 
   ngOnInit() {
