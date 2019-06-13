bd877afb643140542a8bc1841c0687f06b2206b9
archie paredes <archieparedes@gmail.com>
2019-06-13 11:57:49 -0500

    Homepage-login-updates

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -34,6 +34,7 @@ export class HomepageComponent implements OnInit {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
   }
+
   calculate(weight:number, rep:number){
     this.total = weight * (1 + rep/30);
   }
