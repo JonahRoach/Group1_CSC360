2f03ff78e5e59d8a39879148177ccbfee1f0694c
JonahRoach <36675649+JonahRoach@users.noreply.github.com>
2019-06-13 23:00:27 -0600

    Prod Build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCmGl-zGefcQ-2aUgz6j3-zqM44mO_Eo5E",
+    authDomain: "workoutapp-d5252.firebaseapp.com",
+    databaseURL: "https://workoutapp-d5252.firebaseio.com",
+    projectId: "workoutapp-d5252",
+    storageBucket: "workoutapp-d5252.appspot.com",
+    messagingSenderId: "786364091174",
+    appId: "1:786364091174:web:e1b38c6363166427"
+  }
 };
